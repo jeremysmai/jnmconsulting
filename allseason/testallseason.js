95c855a7199855d2b9ad682a418803de7e5dd0b8
jeremysmai <contact@phantominc.net>
2021-11-10 13:35:10 -0600

    progress

--- allseason/testallseason.js
+++ allseason/testallseason.js
@@ -6,7 +6,19 @@ for (i=0, i<array.size(),i++) {
     //Get each separate object into a JSONObject
     JSONObject obj = array.getJSONObject(i);
     //let say you want to modify id for example
-    obj.put("id", 12345);
+    obj.put("FirstName", 12345);
+    obj.put("LastName", 12345);
+    obj.put("PhoneNumber", 12345);
+    obj.put("PostalAddress.AddressFirstLine", 12345);
+    obj.put("PostalAddress.AddressSecondLine", 12345);
+    obj.put("PostalAddress.City", 12345);
+    obj.put("PostalAddress.State", 12345);
+    obj.put("PostalAddress.PostalCode", 12345);
+    obj.put("Email", 12345);
+    obj.put("Source",);
+    obj.put("Description", 12345);
+    obj.put("Category", 12345);
+    obj.put("Urgency", 12345);
     //push the modifyed object into your new array
     newArray.put(obj);
 }
